1b1ad471a07b25e9e8ef875a2969c3b3960b2b47
I-M-Marinov <lcfrrr@gmail.com>
2023-10-28 01:08:54 +0300

    Add project files.

---
+++ PersonsInfo/Team.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            Name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+
+        public string Name { get; private set; }
+        public IReadOnlyCollection<Person> FirstTeam => firstTeam.AsReadOnly();
+        public IReadOnlyCollection<Person> ReserveTeam => reserveTeam.AsReadOnly();
+
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+    }
+}
